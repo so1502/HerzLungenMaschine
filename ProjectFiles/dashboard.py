a0fcb37bed813853712cd0f86ec59b67dd57b1b1
so1502 <so1502@mci4me.at>
2022-05-23 09:07:51 +0200

    Finalized1

--- ProjectFiles/dashboard.py
+++ ProjectFiles/dashboard.py
@@ -1,4 +1,5 @@
 from cmath import nan
+from enum import auto
 from msilib.schema import CheckBox
 from tempfile import SpooledTemporaryFile
 from tkinter import Checkbutton
@@ -60,8 +61,6 @@ fig3 = px.line(df, x="Time (s)", y = "Blood Flow (ml/s)")
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1(children='Cardiopulmonary Bypass Dashboard'),
-#app.layout = html.Div(children=[
-   # html.H1(children='Cardiopulmonary Bypass Dashboard'),
 
     html.Div(children='''
         Bitte wählen Sie zwischen Ihren Patienten aus. 
@@ -104,12 +103,23 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     dcc.Graph(
         id='dash-graph3',
         figure=fig3
-    )
+    ),
+    dcc.Textarea(
+        id='text-area',
+        disabled=True, #disabled --> User cannot interact with textarea
+        style={"width": "100%", 'height': 'auto'}
+    ),
+    dcc.ConfirmDialog(
+        id='confirm-oddities',
+        message="Oddities! Blood flow was too high or low!"#Message to user
+    ),
+    dcc.Dropdown(['Oddities'], id='dropdown-oddities'), #Select Oddities
+    html.Div(id='output-oddities')
 ])
 ### Callback Functions ###
 ## Graph Update Callback
 @app.callback(
-    # In- or Output('which html element','which element property')
+    # left: html element, right: element property
     Output('dash-graph0', 'figure'),
     Output('dash-graph1', 'figure'),
     Output('dash-graph2', 'figure'),
@@ -117,8 +127,8 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     Input('checklist-algo','value')
 )
 def update_figure(value, algorithm_checkmarks):
-    print("Current Subject: ",value)
-    print("current checked checkmarks are: ", algorithm_checkmarks)
+    #print("Current Subject: ",value)
+    #print("current checked checkmarks are: ", algorithm_checkmarks)
     ts = list_of_subjects[int(value)-1].subject_data
     #SpO2
     fig0 = px.line(ts, x="Time (s)", y = data_names[0])
@@ -149,7 +159,7 @@ def update_figure(value, algorithm_checkmarks):
     #Wenn max oder min angeklickt werden, dann werden Traces mit den jeweiligen Werten eingefügt.
    
     grp = ts.agg(['max', 'min', 'idxmax', 'idxmin'])
-    print(grp)
+    #print(grp)
    
     if algorithm_checkmarks is not None:
         if 'max' in algorithm_checkmarks:
@@ -172,12 +182,12 @@ def update_figure(value, algorithm_checkmarks):
     return fig0, fig1, fig2
  
 
-## Blodflow Simple Moving Average Update
+## Blod flow Simple Moving Average Update
 @app.callback(
-    # In- or Output('which html element','which element property')
     Output('dash-graph3', 'figure'),
+    Output('text-area', 'value'),
     Input('subject-dropdown', 'value'),
-    Input('checklist-bloodflow','value')
+    Input('checklist-bloodflow','value'),
 )
 
 def bloodflow_figure(value, bloodflow_checkmarks):
@@ -188,9 +198,9 @@ def bloodflow_figure(value, bloodflow_checkmarks):
     fig3 = px.line(bf, x="Time (s)", y="Blood Flow (ml/s)")
 
     ## Calculate Simple Moving Average: Aufagbe 2
-    if bloodflow_checkmarks is not None:
+    if bloodflow_checkmarks is not None: #behebt Fehler: Nonetype object not iterable
         if bloodflow_checkmarks == ["SMA"]:
-            bf["Blood Flow (ml/s) - SMA"] = ut.calculate_SMA(bf["Blood Flow (ml/s)"],5) 
+            bf["Blood Flow (ml/s) - SMA"] = ut.calculate_SMA(bf["Blood Flow (ml/s)"],4) 
             fig3 = px.line(bf, x="Time (s)", y="Blood Flow (ml/s) - SMA")
 
         if bloodflow_checkmarks == ["CMA"]:
@@ -202,22 +212,84 @@ def bloodflow_figure(value, bloodflow_checkmarks):
     avg = bf.mean()
     x = [0, 480]
     y = avg.loc['Blood Flow (ml/s)']
+    bf["Blood Flow (ml/s) - SMA"] = ut.calculate_SMA(bf[data_names[1]],4) 
+    bf_SMA =  bf["Blood Flow (ml/s) - SMA"]
+
     fig3.add_trace(go.Scatter(x = x, y= [y,y], mode = 'lines', name = 'Mittelwert'))
 
-    #Intervalle,
+    #Intervalle
     y_oben = (avg.loc['Blood Flow (ml/s)'])*1.15
-    fig3.add_trace(go.Scatter(x = x, y= [y_oben,y_oben], mode = 'lines', marker_color = 'blue', name = 'obere Grenze'))
-    
     y_unten = (avg.loc['Blood Flow (ml/s)'])*0.85
-    fig3.add_trace(go.Scatter(x = x, y= [y_unten, y_unten], mode = 'lines', marker_color = 'blue', name = 'untere Grenze'))
-   
+    
+
+    def show_limits(fig):
+
+        fig.add_trace(go.Scatter(x = x, y = [y_oben, y_oben], mode = 'lines', name = 'Upper Limit', line_color='blue')) #adding trace of upper limit to fig3
+        fig.add_trace(go.Scatter(x = x, y = [y_unten, y_unten], mode = 'lines', name = 'Lower Limit', line_color='blue')) #adding trace of lower limit to fig3
+    
+
+        return fig
+
+    ## Update fig3 via checkboxes
+    if bloodflow_checkmarks is not None:
+
+        if 'CMA' in bloodflow_checkmarks and 'SMA' not in bloodflow_checkmarks: #only if CMA is checked
+            
+            bf["Blood Flow (ml/s) - CMA"] = ut.calculate_CMA(bf[data_names[1]],2)
+            fig_CMA = px.line(bf, x="Time (s)", y="Blood Flow (ml/s) - CMA") #update fig3 to show Cumulative moving Average
+
+            if 'Show Limits' in bloodflow_checkmarks: #Check if Limits is checked
+                fig3 = show_limits(fig_CMA) #call show_limits to add traces to current figure
+            
+            fig3 = fig_CMA #save edited figure with or without traces to fig3
+
+
+        if 'SMA' in bloodflow_checkmarks and 'CMA' not in bloodflow_checkmarks: #only if SMA is checked
+
+            fig_SMA = px.line(bf, x="Time (s)", y=bf_SMA) #save plot of edited figure
+
+            if 'Show Limits' in bloodflow_checkmarks:
+                fig3 = show_limits(fig_SMA) #call show_limits to add traces to current figure
+            
+            fig3 = fig_SMA #save edited figure with or without traces to fig3
+
+
+        if bloodflow_checkmarks == ['Show Limits']: #check if Show Limits is the only checked box
+
+            fig3 = show_limits(fig3) #save figure with added limit traces
+    
     fig3.update_layout(
         plot_bgcolor=colors['background'],
         paper_bgcolor=colors['background'],
         font_color=colors['text']
     )   
-    return fig3
+    
+        ## Aufgabe 3.3
+    alert_count = [] # 
+    alert_sum = 0 #int, holds count of invalid values
 
+    for i in bf_SMA:
+        if i > y_oben or i < y_unten: # Value of SMA over oder under limit
+            alert_count.append(bf.index[bf_SMA==i].tolist()) # append list of invalid values to list
+            alert_sum += 1 #for each invalid value, alert_sum is going up by 1
+
+    #print('Alert count: ' + str(alert_count))
+    #print(str(alert_sum))
+
+    # message
+    info_msg = 'Blood flow was not between intervals for' + str(alert_sum) + ' seconds!'
+
+    
+    return fig3, info_msg
+
+@app.callback(
+Output('confirm-oddities', 'displayed'),
+Input('dropdown-oddities', 'value'))
+def display_confirm(value):
+    if value == 'Oddities': #If Oddities selected
+        return True
+    return False
+# dcc.ConfirmDialogProvider for an easier way to display an alert when clicking on an item.
 
 if __name__ == '__main__':
     app.run_server(debug=True)
